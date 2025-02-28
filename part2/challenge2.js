fab990cd0ce2dc7327abba5c2bd79fa078aa9d02
Prerna Singh <prernasingh.xa@gmail.com>
2025-02-28 23:50:40 +0530

    Challenge no. 2 on Conditions

--- part2/challenge2.js
+++ part2/challenge2.js
@@ -1,1 +1,11 @@
-// Checking if a string is equal to another string
+// CHECKING IF A STRING IS EQUAL TO ANOTHER STRING
+
+let string1 = "Prerna";
+let string2 = "Prerna";
+
+if(string1 == string2){
+    console.log("The strings are equal to each other");
+}
+else{
+    console.log("The strings are not equal to one another");
+}
