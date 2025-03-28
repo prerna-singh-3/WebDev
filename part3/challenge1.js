e3fc0dafb9474b3d3aa948c5b9c811340a8b4537
Prerna Singh <prernasingh.xa@gmail.com>
2025-03-28 17:27:17 +0530

    ..

--- part3/challenge1.js
+++
@@ -1,7 +0,0 @@
-// Declare an array named 'teaFlavours' that contains the strings '"green tea"', '"black tea"',and '"oolong tea"',
-// Access the first element of the array and store it in a variable named 'firstTea'.
-
-let teaFlavours = ["green tea", "black tea", "oolong tea"]
-
-let firstTea = teaFlavours[0];
-console.log(firstTea);
