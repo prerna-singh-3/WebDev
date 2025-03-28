fb9d9520641beeb34b7bfbc1a65d35f2cc7ac51c
Prerna Singh <prernasingh.xa@gmail.com>
2025-03-28 19:51:25 +0530

    Array Challenge 5

---
+++ Array-Challenges/challenge5.js
@@ -0,0 +1,5 @@
+// You have an array named 'teaOrders' with '"chai"', '"iced tea"', '"matcha"', and '"pearl grey"'. Remove the last element of the array using the 'pop' method and store it in a variable named 'lastOrder'.
+
+let teaOrders = ["chai", "iced", "matcha", "pearl grey"]
+const lastOrder = teaOrders.pop("pearl grey");
+console.log(lastOrder);
