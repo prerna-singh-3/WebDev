4a5cf83701f1b619eebecf1886ad5703c53e95fd
Prerna Singh <prernasingh.xa@gmail.com>
2025-03-28 17:26:37 +0530

    Array Challenge 2

---
+++ Array-Challenges/challenge2.js
@@ -0,0 +1,6 @@
+// Declare an array named 'cities' containing '"London"', '"Tokyo"','"Paris"', and '"New York"', 
+// Access the third element in the array and store it in a variable named 'favouriteCity'.
+
+let cities = ["London", "Tokyo", "Paris", "New York"];
+const favouriteCity = cities[2];
+console.log(favouriteCity);
