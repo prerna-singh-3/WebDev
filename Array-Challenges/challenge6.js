df552c88440f6d56080889a5465268e683899777
Prerna Singh <prernasingh.xa@gmail.com>
2025-03-28 20:10:22 +0530

    Array Challenge 6

---
+++ Array-Challenges/challenge6.js
@@ -0,0 +1,5 @@
+// You have an array named 'popoularTeas' containing '"green tea"', '"oolong tea"', and '"chai"'. Create a soft copy of this array named 'softCopyTeas'.
+
+let popoularTeas = ["green tea", "oolong tea", "chai"];
+let softCopyTeas = popoularTeas
+console.log(softCopyTeas);
