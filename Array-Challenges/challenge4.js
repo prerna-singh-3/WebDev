45051a872fcaeb9db21d5755eef70202c8e4aeee
Prerna Singh <prernasingh.xa@gmail.com>
2025-03-28 18:52:18 +0530

    Array Challenge 4

---
+++ Array-Challenges/challenge4.js
@@ -0,0 +1,5 @@
+// Declare an array named 'citiesVisited' containing '"Mumbai"' and '"Sydney"'. Add '"Berlin"' to the array using the 'push' method.
+
+let citiesVisited = ["Mumbai", "Sydney"]
+citiesVisited.push("Berlin");
+console.log(citiesVisited);
