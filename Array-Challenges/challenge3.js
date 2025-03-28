0d171b764ad8ae1f1fca804200a0d28d2a72cfcc
Prerna Singh <prernasingh.xa@gmail.com>
2025-03-28 18:36:26 +0530

    Array Challenge 3

---
+++ Array-Challenges/challenge3.js
@@ -0,0 +1,6 @@
+// You have an array named 'teaTypes' containing '"herbal tea"', '"white tea"', and '"masala chai"'. Change the second element of array to '"jasmine tea"'.
+
+let teaTypes = ["herbal tea", "white tea", "masala chai"]
+teaTypes[2] = "jasmine tea";
+console.log(teaTypes);
+
