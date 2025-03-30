09631820721611bd89651bf8d8bc12a40cfda230
Prerna Singh <prernasingh.xa@gmail.com>
2025-03-30 20:28:19 +0530

    Loop Challenge 4

---
+++ Loop-Challenges/challenge4.js
@@ -0,0 +1,9 @@
+// Write a 'do while' loop that adds numbers from 1 to 3 and stores the result in a variable named 'total'.
+
+let total = 0
+let k =1;
+do {
+    total += k;
+    k++;
+} while (k <= 3);
+console.log(total);
