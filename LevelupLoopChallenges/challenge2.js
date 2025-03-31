f801e422bc80fb24a55fce79002891f392f9db8d
Prerna Singh <prernasingh.xa@gmail.com>
2025-03-31 15:11:44 +0530

    Levelup Challenge 2

---
+++ LevelupLoopChallenges/challenge2.js
@@ -0,0 +1,11 @@
+// Write a 'for' loop that loops through the array '["London", "New York", "Paris", "Berlin"]' and skips '"Paris"'. Store the other cities in a new array named 'visitedCities'
+
+let cities = ["London", "New York", "Paris", "Berlin"]
+let visitedCities = [];
+for (let i = 0; i < cities.length; i++) {
+    if(cities[i] === "Paris"){
+        continue;
+    }
+    visitedCities.push(cities[i]);
+}
+console.log(visitedCities);
