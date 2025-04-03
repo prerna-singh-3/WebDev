a64799d569bbfc6d64d2de4d91b62c33d421b523
Prerna Singh <prernasingh.xa@gmail.com>
2025-04-03 20:07:03 +0530

    Function part 4

---
+++ Functions/part4.js
@@ -0,0 +1,12 @@
+// Write a function named 'processTeaOrder' that takes another function, 'makeTea', as a parameter and calls it with the argument '"earl grey"'. Return the result of calling 'makeTea'.
+
+function makeTea(typeOfTea){
+    return `maketea: ${typeOfTea}`;
+}
+
+function processTeaOrder(teaFunction){
+   return teaFunction('earl grey')
+}
+
+let order = processTeaOrder(makeTea)
+console.log(order);
