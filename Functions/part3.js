f7eb6f0d115cd2615ca8268515c7a52831b6c3f4
Prerna Singh <prernasingh.xa@gmail.com>
2025-04-03 19:22:32 +0530

    Function part 3

---
+++ Functions/part3.js
@@ -0,0 +1,7 @@
+// Write an arrow function named 'calculateTotal' that takes two parameters: 'price' and 'quantity'. The function should return the total cost by multiplying the 'price' and 'quantity'.
+
+const calculateTotal = (price, quantity) => {
+return price * quantity  
+}            
+let totalCost = calculateTotal(499 , 100)
+console.log(totalCost);
