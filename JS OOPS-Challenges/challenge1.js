f32a5bf95ffabc5867663c00605a109852b37b3f
Prerna Singh <prernasingh.xa@gmail.com>
2025-04-07 20:32:38 +0530

    Prototype Chaining

---
+++ JS OOPS-Challenges/challenge1.js
@@ -0,0 +1,8 @@
+// Task: Prototype Chaining
+
+// Create a constructor function Animal that has a method speak() that return 'Animal speaking'.
+
+// Then create another constructor Dog that inherits from Animal using prototypes.
+
+// The Dog constructor should add a method bark() that returns 'Woof!'. Demonstrate the prototype chain between Dog and Animal
+
